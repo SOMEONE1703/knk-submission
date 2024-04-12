fbb041a2bc1f4598f675150fcd260df0db9c2be8
SOMEONE1703 <siphelel.1703@gmail.com>
2024-04-12 12:57:09 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+const path=require("path");
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const filePath = path.join(__dirname, "../public/index.html");
+  res.sendFile(filePath);
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const path=require("path");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+  const filePath = path.join(__dirname, "../public/login.html");
+  res.sendFile(filePath);
+});
+
+router.get('/register', function(req, res, next) {
+  const filePath = path.join(__dirname, "../public/register.html");
+  res.sendFile(filePath);
 });
 
 module.exports = router;
