3f0141d609ef8927b837bedf2b5c6362e9c3feff
SOMEONE1703 <siphelel.1703@gmail.com>
2024-04-18 18:50:30 +0200

    something

--- public/javascripts/index_script.js
+++ public/javascripts/index_script.js
@@ -1,11 +1,11 @@
 function switch_login() {
-    window.location.href = "https://knk-submission.azurewebsites.net/users/login";
+    window.location.href = "/users/login";
 }
 
 function switch_register() {
-    window.location.href = "https://knk-submission.azurewebsites.net/users/register";
+    window.location.href = "/users/register";
 }
 
 function switch_leaderboard() {
-    window.location.href = "https://knk-submission.azurewebsites.net/leaderboard";
+    window.location.href = "/leaderboard";
 }
